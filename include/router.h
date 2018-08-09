2535e435f76c2fd39d29cbedde04042b6a4d460a
Mario Castro Morera <mariocastromorera@gmail.com>
2018-08-09 02:27:25 -0600

    Changes various
    - Moved inline functions to constants header file
    - "Coding Style"
    - Improved debug messages
    - Added Router 7
    - Added define

--- include/router.h
+++ include/router.h
@@ -11,25 +11,35 @@
 class Router : public sc_core::sc_module
 {
     public:
-
+        // *********************************************
+        // Constructor and Methods
+        // *********************************************
         SC_HAS_PROCESS(Router);
         Router(sc_core::sc_module_name module_name, unsigned int id);
 
-        virtual tlm::tlm_sync_enum nb_transport_fw(int id, tlm::tlm_generic_payload& trans, tlm::tlm_phase& phase, sc_time& delay);
-        virtual tlm::tlm_sync_enum nb_transport_bw(int id, tlm::tlm_generic_payload& trans, tlm::tlm_phase& phase, sc_time& delay);
+        virtual tlm::tlm_sync_enum nb_transport_fw(             int id,
+                                                                tlm::tlm_generic_payload& trans,
+                                                                tlm::tlm_phase& phase, sc_time& delay);
 
+        virtual tlm::tlm_sync_enum nb_transport_bw(             int id,
+                                                                tlm::tlm_generic_payload& trans,
+                                                                tlm::tlm_phase& phase, sc_time& delay);
+        // *********************************************
+        // Class attributes
+        // *********************************************
         tlm_utils::multi_passthrough_target_socket<Router>      target_socket;
         tlm_utils::multi_passthrough_initiator_socket<Router>   init_socket;
 
     private:
+        // *********************************************
+        // Class attributes
+        // *********************************************
         const sc_time 					                        LATENCY;
+        int                                                     router_id;
 
         std::map <tlm::tlm_generic_payload*, unsigned int>      m_id_map;
 
-        inline unsigned int decode_address(sc_dt::uint64 address, sc_dt::uint64& masked_address);
-        inline sc_dt::uint64 compose_address(unsigned int target_nr, sc_dt::uint64 address);
 
-        int                                                    router_id;
 };
 
 #endif
